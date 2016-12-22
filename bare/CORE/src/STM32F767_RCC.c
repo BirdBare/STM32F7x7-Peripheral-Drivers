cc0f1263a24a842ae528daea50bf137e377620bb
Bradley Bare <bradleybare@gmail.com>
2016-12-22 12:10:12 -0600

    Changed RCC functions to void to prevent warnings. Made typedef called structxo to represent any possible xo object being passed to functions. I normally will NOT use typedef.

--- bare/CORE/src/STM32F767_RCC.c
+++ bare/CORE/src/STM32F767_RCC.c
@@ -12,12 +12,12 @@
 //										 
 //	
 //******************************************************************************
-uint32_t RCC_EnableClock(struct RCCxo *RCCo)
+uint32_t RCC_EnableClock(structxo *RCCo)
 {
-	uint32_t setbit = 1 << RCCo->clockbitoffset;
-	uint32_t prevstate = *RCCo->clockreg & setbit;
+	uint32_t setbit = 1 << ((struct RCCxo *)RCCo)->clockbitoffset;
+	uint32_t prevstate = *((struct RCCxo *)RCCo)->clockreg & setbit;
 
-	*RCCo->clockreg |= setbit;
+	*((struct RCCxo *)RCCo)->clockreg |= setbit;
 	
 	return prevstate;
 }
@@ -27,21 +27,22 @@ uint32_t RCC_EnableClock(struct RCCxo *RCCo)
 //										 
 //	
 //******************************************************************************
-uint32_t RCC_DisableClock(struct RCCxo *RCCo)
+uint32_t RCC_DisableClock(structxo *RCCo)
 {
-	uint32_t resetbit = 1 << RCCo->clockbitoffset;
-	uint32_t prevstate = *RCCo->clockreg & resetbit;
+	uint32_t resetbit = 1 << ((struct RCCxo *)RCCo)->clockbitoffset;
+	uint32_t prevstate = *((struct RCCxo *)RCCo)->clockreg & resetbit;
 
-	*RCCo->clockreg &= ~resetbit;
+	*((struct RCCxo *)RCCo)->clockreg &= ~resetbit;
 	
 	return !prevstate;
 }
 
-void RCC_Reset(struct RCCxo *RCCo)
+void RCC_Reset(structxo *RCCo)
 {
-	uint32_t bit = 1 << RCCo->clockbitoffset;
+	uint32_t bit = 1 << ((struct RCCxo *)RCCo)->clockbitoffset;
 
-	uint32_t *resetreg = (void*)((uint32_t)RCCo->clockreg -0x20);
+	uint32_t *resetreg = 
+		(void*)((uint32_t)((struct RCCxo *)RCCo)->clockreg -0x20);
 
 	*resetreg |= bit;
 	for(int i = 10; i != 0; i--)
